1667a2bf259332a928a3d10d21df088edf8b0eac
sajjmon <86625235+sajjmon@users.noreply.github.com>
2022-03-09 15:27:15 +0100

    Add files via upload

--- store.js
+++ store.js
@@ -1,13 +1,55 @@
-var removeCartItembuttons = document.getElementsByClassName('btn-danger')
-console.log(removeCartItembuttons)
-for (var i = 0; i < removeCartItembuttons.length; i++) {
-    var button = removeCartItembuttons[i]
-    button.addEventListener('click', function(event) {
-        var buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.remove()
-        updateCartTotal()
-    })
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
 }
+
+function ready() {
+    var removeCartItembuttons = document.getElementsByClassName('btn-danger')
+    console.log(removeCartItembuttons)
+    for (var i = 0; i < removeCartItembuttons.length; i++) {
+        var button = removeCartItembuttons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+
+    }
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+
+    }
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+}
+
+
+
 
 function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
@@ -23,5 +65,7 @@ function updateCartTotal() {
 
 
     }
-    document.getElementsByClassName('cart-total-price')[0].innerText = total
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$'
+    total
 }
