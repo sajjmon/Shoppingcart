8122597298cd347c5efc80be3f39070a59332b90
sajjmon <86625235+sajjmon@users.noreply.github.com>
2022-03-11 13:59:33 +0100

    Add files via upload

--- store.js
+++ store.js
@@ -24,6 +24,16 @@ function ready() {
         button.addEventListener('click', addToCartClicked)
 
     }
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+function purchaseClicked() {
+    alert('Thank you for your purchase')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
 }
 
 function removeCartItem(event) {
